2256a502a77805212d9476af94523ffea74a8c64
hoghanh <hanhchse150754@fpt.edu.vn>
2023-07-13 09:19:36 +0700

    update config

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  APP_API_URL: "http://192.168.1.45:8080",
-  LOCALSTORAGE_TOKEN_NAME: "jwtPvoc2HNdpGxPJhaoNNRoxveolp4KNi",
-};
-
-export const { APP_API_URL, LOCALSTORAGE_TOKEN_NAME } = config;
